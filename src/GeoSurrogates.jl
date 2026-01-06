cdc785ad386f15a0b3630a6ad707a90278ef0589
Josh Day <emailjoshday@gmail.com>
2026-01-06 14:15:35 -0500

    lots of changes... RasterSurrogate -> RasterWrap, etc.

--- src/GeoSurrogates.jl
+++ src/GeoSurrogates.jl
@@ -1,96 +1,129 @@
 module GeoSurrogates
 
-using Extents, Rasters, Interpolations
+using ADTypes, ArchGDAL, CategoricalArrays, DataFrames, Extents, Interpolations, Images, LazyArrays, Lux, Optimisers, Random, Rasters, RasterDataSources, Statistics, StatsAPI, StatsModels, StyledStrings, Tables
 
+import Extents
 import GeoInterface as GI
 import GeometryOps as GO
+import DimensionalData as DD
 
-#-----------------------------------------------------------------------------# features
-function features(r::Raster)
-    itr = Iterators.product(dims(r)...)
-    out = Dict{Symbol, Vector{Union{eltype.(dims(r))...}}}()
-    for i in 1:ndims(r)
-        out[name(dims(r, i))] = vec(map(x -> x[i], itr))
+import StatsAPI: predict, fit!
+
+export ImplicitTerrain
+
+
+#-----------------------------------------------------------------------------# normalize
+# *normalize* in this package means scaling to [-1, 1]
+normalize(x::AbstractArray{<:Number}, a = minimum(x), b = maximum(x)) = 2 * (x .- a) ./ (b - a) .- 1
+
+# Fallback does nothing (e.g. for strings)
+normalize(x) = x
+
+normalize(dim::DD.Dimension) = DD.Lookups.iscategorical(dim) ? dim : X(normalize(dim.val.data))
+
+normalize(r::Raster) = modify(normalize, r)
+
+normalize(nt::NamedTuple) = map(normalize, nt)
+
+is_normalized(x) = all(-1 .≤ x .≤ 1)
+
+function normalize!(df::DataFrame)
+    for col in names(df)
+        df[!, col] = normalize(df[!, col])
     end
-    return out
+    return df
 end
 
-#-----------------------------------------------------------------------------# utils
-function xyfeatures(r::Raster)
-    x = repeat(dims(r, X), outer=length(r.dims[2]))
-    y = repeat( r.dims[2].val.data, inner=length(r.dims[1]))
-    (; x, y)
+#-----------------------------------------------------------------------------# GeoSurrogate
+abstract type GeoSurrogate end
+
+Base.show(io::IO, o::T) where {T <: GeoSurrogate} = print(io, T.name.name)
+
+# Interface:
+GI.crs(::GeoSurrogate) = nothing
+GI.extent(::GeoSurrogate) = nothing
+
+predict(model::GeoSurrogate, x::Tuple) = error("predict not implemented for $(typeof(model)) with input type Tuple")
+predict(model::GeoSurrogate, x::Raster) = error("predict not implemented for $(typeof(model)) with input type Raster")
+
+
+# #-----------------------------------------------------------------------------# LinReg
+# struct LinReg{F <: FormulaTerm, T <: Number} <: GeoSurrogate
+#     formula::F
+#     β::Vector{T}
+# end
+
+# function LinReg(r::Raster)
+#     nt = merge(features(r), (; data = data(r)))
+#     formula = fullfactorialformula(r)
+#     f = apply_schema(formula, schema(formula, nt))
+#     y, x = modelcols(f, nt)
+#     β = x \ y
+#     LinReg(f, β)
+# end
+
+# function (o::LinReg)(r::Raster)
+#     X = modelmatrix(o.formula, features(r))
+#     ẑ = X * o.β
+#     Raster(reshape(ẑ, size(r)), dims=DD.dims(r))
+# end
+
+# function (o::LinReg)(x)
+#     if Tables.istable(x)
+#         X = modelmatrix(o.formula, x)
+#         return X * o.β
+#     else  # assume single row
+#         only(o(Tables.rowtable((x, ))))
+#     end
+# end
+
+
+#-----------------------------------------------------------------------------# RasterWrap
+struct RasterWrap{R <: Raster, A, E, F} <: GeoSurrogate
+    raster::R
+    interpolations_alg::A
+    extrapolation_alg::E
+    f::F
+    function RasterWrap(r::Raster, int = BSpline(Linear()), ext = nothing)
+        r = forwardorder(r)
+        itp = interpolate(r.data, int)
+        itps = scale(itp, (x.val.data for x in r.dims)...)
+        f = isnothing(ext) ? itps : extrapolate(itps, ext)
+        return new{typeof(r), typeof(int), typeof(ext), typeof(f)}(r, int, ext, f)
+    end
 end
+
+predict(rw::RasterWrap, coords::Tuple) = rw.f(coords...)
+predict(rw::RasterWrap, r::Raster) = nothing
+
+GI.crs(o::RasterWrap) = GI.crs(o.raster)
+GI.extent(o::RasterWrap) = GI.extent(o.raster)
+
+function forwardorder(r::Raster)
+    for (i, dim) in enumerate(r.dims)
+        if step(dim.val.data) < 0
+            r = reverse(r, dims=i)
+        end
+    end
+    return r
+end
+
+
+#-----------------------------------------------------------------------------# GeomWrap
+struct GeomWrap{G, K} <: GeoSurrogate
+    geometry::G
+    kernel::K
+    GeomWrap(geom::G, kern::K = Base.Fix2(gaussian, 4)) where {G, K} = new{G,K}(geom,kern)
+end
+
+(o::GeomWrap)(x...) = o.kernel(GO.distance(x, o.geometry))
 
 gaussian(u, k) = exp(-0.5 * (k * u) ^ 2)
 
-# Return function for best-fit plane
-function plane(r::Raster)
-    z = vec(r)
-    x, y = xyfeatures(r)
-    β = hcat(x, y, x .* y) \ z
-    (x, y) -> x * β[1] + y * β[2] + x * y * β[3]
-end
+GI.crs(o::GeomWrap) = GI.crs(o.geometry)
+GI.extent(o::GeomWrap) = GI.extent(o.geometry)
 
-#-----------------------------------------------------------------------------# AbstractGeoSurrogate
-abstract type AbstractGeoSurrogate end
-
-function Base.show(io::IO, o::T) where {T <: AbstractGeoSurrogate}
-    print(io, T.name.name, " | ", GI.crs(o), " | ", GI.extent(o))
-end
-
-# Interface:
-# GI.crs(::AbstractGeoSurrogate) = nothing
-# GI.extent(::AbstractGeoSurrogate) = nothing
-
-
-#-----------------------------------------------------------------------------# NoExtrapolation
-struct NoExtrapolation end
-
-#-----------------------------------------------------------------------------# RasterSurrogate
-struct RasterSurrogate{F, C, E <: Extent} <: AbstractGeoSurrogate
-    f::F
-    crs::C
-    extent::E
-    function RasterSurrogate(r::Raster, alg = BSpline(Linear()), ext = NoExtrapolation())
-        any(ismissing, r) && error("surrogates do not allow missing data")
-        crs = GI.crs(r)
-        extent = GI.extent(r)
-        for (i, dim) in enumerate(r.dims)
-            if step(dim.val.data) < 0
-                r = reverse(r, dims=i)
-            end
-        end
-        itp = interpolate(r.data, alg)
-        itps = scale(itp, (x.val.data for x in r.dims)...)
-        f = ext == NoExtrapolation() ? itps : extrapolate(itps, ext)
-        return new{typeof(f), typeof(crs), typeof(extent)}(f, crs, extent)
-    end
-end
-GI.crs(o::RasterSurrogate) = GI.crs(o.r)
-GI.extent(o::RasterSurrogate) = o.extent
-(o::RasterSurrogate)(x...) = o.f(x...)
-
-
-#-----------------------------------------------------------------------------# GeometrySurrogate
-struct GeometrySurrogate{F, T, C, E} <: AbstractGeoSurrogate
-    f::F
-    geomtrait::T
-    crs::C
-    extent::E
-end
-function GeometrySurrogate(geom, k)
-    trt = GI.geomtrait(geom)
-    f = get_fun(trt, geom, k)
-    GeometrySurrogate(f, trt, GI.crs(geom), GI.extent(geom))
-end
-GI.crs(o::GeometrySurrogate) = o.crs
-GI.extent(o::GeometrySurrogate) = o.extent
-(o::GeometrySurrogate)(x...) = o.f(x...)
-
-# Fallback
-get_fun(::Any, geom, k) = (x, y) -> gaussian(GO.distance((x,y), geom), k)
-
-
+#-----------------------------------------------------------------------------# ImplicitTerrain
+include("ImplicitTerrain.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,88 @@
 using GeoSurrogates
-using RasterDataSources
+using RasterDataSources, DataFrames, Rasters, ArchGDAL, Zygote, ProgressMeter
 using Test
+using StatsAPI: predict, fit!
 
-@testset "GeoSurrogates.jl" begin
-    # Write your tests here.
+import GeoInterface as GI
+
+using p7zip_jll: p7zip
+
+
+#-----------------------------------------------------------------------------# Test Data
+url = "https://github.com/user-attachments/files/24169167/jl_d1CIih.zip"
+
+function extract(file::AbstractString, dir::AbstractString = mktempdir())
+    run(`$(p7zip()) x $file -o$dir -y`)
+    return only(readdir(dir, join=true))
 end
+
+dir = extract(Base.download(url))
+
+files = readdir(dir, join=true)
+
+tif_file = only(filter(f -> endswith(f, ".tif"), files))
+
+r = Raster(tif_file)
+
+elev = let
+    i = findfirst(==("US_ELEV2020"), r.dims[3].val)
+    r[Band = i]
+end
+
+veg = let
+    i = findfirst(==("US_250FBFM13"), r.dims[3].val)
+    r[Band = i]
+end
+
+#-----------------------------------------------------------------------------# normalize
+@testset "normalize" begin
+    df = dropmissing!(DataFrame(elev))
+    df2 = GeoSurrogates.normalize!(df)
+    for col in names(df2)
+        @test GeoSurrogates.is_normalized(df2[!, col])
+    end
+end
+
+#-----------------------------------------------------------------------------# RasterWrap
+@testset "RasterWrap" begin
+    rw = GeoSurrogates.RasterWrap(elev)
+    @test GI.crs(rw) == GI.crs(elev)
+    @test GI.extent(rw) == GI.extent(elev)
+    for (coord, data) in zip(DimPoints(elev), elev)
+        @test predict(rw, coord) == data
+    end
+end
+
+#-----------------------------------------------------------------------------# GeomWrap
+@testset "GeomWrap" begin
+    gw = GeoSurrogates.GeomWrap(GI.Point(0.0, 0.0))
+    @test gw(0, 0) == 1.0
+    @test gw(1, 0) ≈ GeoSurrogates.gaussian(1.0, 4)
+end
+
+
+#-----------------------------------------------------------------------------# ImplicitTerrain
+@testset "ImplicitTerrain" begin
+    m = ImplicitTerrain.Model()
+
+    predict(m, (0, 0))
+
+    predict(m, elev)
+end
+
+
+
+# t = GeoSurrogates.ImplicitTerrainTrainer(raster = elev)
+
+# @showprogress for _ in 1:10
+#     GeoSurrogates.train_surface_model!(t; steps=3)
+# end
+
+# R = t.pyramid[1]
+# x, y = GeoSurrogates.xyfeatures(R)
+
+# XY = Float32.(vcat(x', y'))
+
+# ẑ, st = Lux.apply(t.chain_s.chain, XY, t.chain_s.parameters, t.chain_s.states)
+
+# Ẑ = reshape(ẑ, size(R))
